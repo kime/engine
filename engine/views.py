7cdc5997a318771e20ee891dc627443a1ec325d0
Achyudh Ram <achyudhk@gmail.com>
2019-06-06 14:00:20 -0400

    Add auth for /enhance endpoint

--- engine/views.py
+++ engine/views.py
@@ -1,13 +1,32 @@
 from traceback import print_stack
 
 from flask import Blueprint, request
+from flask_httpauth import HTTPBasicAuth
+from passlib.handlers.sha2_crypt import sha512_crypt
 
-from engine import models
+from engine import models, config
 from engine.responses import ok, internal_error, not_implemented
 
+auth = HTTPBasicAuth()
 engine = Blueprint('engine', __name__, url_prefix='')
 
 
+@auth.verify_password
+def verify_password(user, secret):
+    """
+
+    :param auth_token:
+    :param password:
+    :return:
+    """
+    # Check if auth_token is valid
+    actual_user, secret_hash = config.backend_credentials()
+    if not user == actual_user or not sha512_crypt.verify(secret, secret_hash):
+        return False
+    return True
+
+
+@auth.login_required
 @engine.route('/enhance', methods=['POST'])
 def enhance():
     try:
